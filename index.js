518e66d4ba8bb3608491966a03c0d100ef6bc207
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-25 09:58:57 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+  const phrase = document.getElementById('text')
+  phrase.textContent = 'This is really cool!'
+})
